41afffc21d384f1f9c54df1c084e9c07f8a5a27f
baharehmarandi <70742323+baharehmarandi@users.noreply.github.com>
2024-10-01 09:57:28 +0330

    initialize components and routes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,16 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { fa_IR, provideNzI18n } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import fa from '@angular/common/locales/fa';
+import { FormsModule } from '@angular/forms';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
+
+registerLocaleData(fa);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideNzI18n(fa_IR), importProvidersFrom(FormsModule), provideAnimationsAsync(), provideHttpClient()]
 };
